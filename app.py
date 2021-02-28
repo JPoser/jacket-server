413da4b4630c2db4afae3cdd509bb96e22be5968
jposer <jposer@release.works>
2021-02-28 13:50:30 +0000

    added check_tweet function to get basic colour

--- app.py
+++ app.py
@@ -11,20 +11,42 @@ access_token_secret = config['twitter']['access_token_secret']
 
 app = Flask(__name__)
 
-colours = [
-    {
-        'id': 1,
-        'colour': 'blue'
-    },
-    {
-        'id': 2,
-        'colour': 'red'
-    }
-]
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 twitter_api = tweepy.API(auth)
+
+colour = ""
+
+def check_tweet(tweet_text):
+    tweet_text = tweet_text.lower()
+    if "red" in tweet_text:
+        return "red"
+    elif "orange" in tweet_text:
+        return "orange"
+    elif "yellow" in tweet_text:
+        return "yellow"
+    elif "chartreuse" in tweet_text:
+        return "chartreuse"
+    elif "green" in tweet_text:
+        return "green"
+    elif "spring" in tweet_text:
+        return "spring"
+    elif "cyan" in tweet_text:
+        return "cyan"
+    elif "azure" in tweet_text:
+        return "azure"
+    elif "blue" in tweet_text:
+        return "blue"
+    elif "violet" in tweet_text:
+        return "violet"
+    elif "violet" in tweet_text:
+        return "violet"
+    elif "magenta" in tweet_text:
+        return "magenta"
+    elif "rose" in tweet_text:
+        return "rose"
+    else:
+        return "white"
 
 def get_latest_mention():
     tweets = twitter_api.mentions_timeline()
@@ -38,7 +60,9 @@ def index():
 
 @app.route('/api/v1.0/get_tweets', methods=['GET'])
 def get_tweets():
-    return jsonify({'colours': colours[0], 'tweets': get_latest_mention()})
+    if check_tweet(get_latest_mention()) != None:
+        colour = check_tweet(get_latest_mention())
+    return jsonify({'colour': colour, 'tweets': get_latest_mention()})
 
 if __name__ == '__main__':
     app.run(debug=True)
